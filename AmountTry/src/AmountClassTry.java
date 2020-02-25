6f665f0da3f283addfb29f4b75061fea61b4e310
subityjerald <subityjerald1994@gmail.com>
2020-02-25 09:15:52 +0530

    amount

---
+++ AmountTry/src/AmountClassTry.java
@@ -0,0 +1,17 @@
+
+public class AmountClassTry {
+	public static void main(String[] args) {
+		 int amount;
+	 amount=(int)5.95;
+	 
+		amount=amount+(int)25.00;
+		System.out.print("we will bill in $");
+		System.out.print(amount);
+		System.out.println(" in credit card");
+		
+		
+		
+		
+
+}
+}
